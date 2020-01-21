c9eb45ecc6bcbafcf2188ff4483b1b5c90372f0a
Suzin <patrycjusz.sosnowski@gmail.com>
2020-01-21 21:57:30 +0100

    add Author Controller

---
+++ src/Form/AuthorType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AuthorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('surname')
+            ->add('countryOfOrigin')
+            ->add('yearOfBirth')
+            ->add('dateOfDead')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
